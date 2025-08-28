6eaad9abc437b315db2133ae857f4240c76c8755
Yadira Cervantes <cervantes.yadira@student.greenriver.edu>
2025-08-28 16:16:14 -0700

    Initialized React project and added non-functional nav

---
+++ portfolio-website/src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div class='navbar'>
+        <img alt='Logo'>
+        </img>
+        <nav>
+          <a class='nav-link'>About</a>
+          <a class='nav-link'>Projects</a>
+          <a class='nav-link'>Resume</a>
+          <a class='nav-link'>Contact</a>
+        </nav>
+      </div>
+      <section>
+        <h1>
+          Hello! I'm
+          <span> Yadira</span>
+          <span>.</span>
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipiscing elit. Dolor sit amet consectetur 
+          adipiscing elit quisque faucibus.
+        </p>
+        <img></img>
+      </section>
+    </>
+  )
+}
+
+export default App
